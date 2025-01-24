4e505715604b8d41f478db6fb4145a2cb78b13eb
MEGUMMY1 <cys10902@nate.com>
2025-01-24 17:33:38 +0900

    Docs: title 지정

--- src/pages/qr-list.tsx
+++ src/pages/qr-list.tsx
@@ -1,5 +1,22 @@
 import QRList from "@/components/QRList/QRList";
+import { serviceUrl } from "@/constants/serviceurl";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { InitialPageMeta } from "@/components/MetaData";
 
 export default function QRListPage() {
-  return <QRList />;
+  const router = useRouter();
+  const [OGTitle] = useState("QR목록 | MOSAICSEOUL WAITING");
+  const [OGUrl, setOGUrl] = useState(serviceUrl);
+
+  useEffect(() => {
+    setOGUrl(serviceUrl + router.asPath);
+  }, [router.asPath]);
+
+  return (
+    <>
+      <InitialPageMeta title={OGTitle} url={OGUrl} />
+      <QRList />
+    </>
+  );
 }
